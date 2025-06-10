c5a7560456d75095dc96711c361f1c9d43667623
Maximilian Johannes Müser <m.mueser@web.de>
2025-06-10 10:23:20 +0200

    Update App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('App-Komponente', () => {
-  it('zeigt den Text "Login"', () => {
-    render(<App />);
-    expect(screen.getByText(/login/i)).toBeInTheDocument();
-  });
+it('zeigt den Login-Button', () => {
+  render(<App />);
+  expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
 });
